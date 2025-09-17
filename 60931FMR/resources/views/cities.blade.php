35264b0db5e34fa783dcb43a6dcd2a11603e0b98
sandajer <sandajer@JinxLover.localdomain>
2025-09-17 11:28:38 +0500

    add city model

---
+++ 60931FMR/resources/views/cities.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>609-31</title>
+</head>
+<body>
+    <h2>Список городов:</h2>
+    <table border="1">
+        <thead>
+            <td>id</td>
+            <td>Наименование</td>
+        </thead>
+        @foreach ($cities as $city)
+        <tr>
+            <td>{{$city->id}}</td>
+            <td>{{$city->name}}</td>
+        </tr>
+        @endforeach
+    </table>
+</body>
+</html>
--- 60931FMR/routes/web.php
+++ 60931FMR/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CityController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,3 +10,5 @@ Route::get('/', function () {
 Route::get('/hello', function () {
     return view('hello', ['title' => 'Hellow World!']);
 });
+
+Route::get('/cities', [CityController::class,'index']);
